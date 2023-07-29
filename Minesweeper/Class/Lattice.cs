3acd13b508e1bb012e727ad102322bb5ade3659f
AIKA <1343707921@qq.com>
2023-07-29 16:34:13 +0800

    更新鼠标按下操作

---
+++ Minesweeper/Class/Lattice.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minesweeper.Class
+{
+  public class Lattice : INotifyPropertyChanged
+  {
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+	private bool pressed;
+
+	public bool Pressed
+	{
+	  get { return pressed; }
+	  set 
+	  {
+		pressed = value; 
+		PropertyChanged?.Invoke(this,new PropertyChangedEventArgs(nameof(Pressed)));
+	  }
+	}
+
+  }
+}
--- Minesweeper/MainWindow.xaml.cs
+++ Minesweeper/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Minesweeper.Class;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
@@ -25,15 +26,17 @@ namespace Minesweeper
     int row = 16;
     int column = 30;
     int maxLattice = 480;
+    bool leftRightBtnPressed = false;
     List<int> offsetList = new List<int>();
-    public ObservableCollection<int> items { get; set; }
-      = new ObservableCollection<int>(new List<int>(480));
+    List<Lattice> lastPressList = new List<Lattice>();
+    public ObservableCollection<Lattice> items { get; set; }
+      = new ObservableCollection<Lattice>(new List<Lattice>(480));
     public MainWindow()
     {
       InitializeComponent();
       for (int i = 0; i < maxLattice; i++)
       {
-        items.Add(i);
+        items.Add(new Lattice());
       }
       ReCalculateOffset();
       DataContext = this;
@@ -55,32 +58,55 @@ namespace Minesweeper
       offsetList.Add(column);
       offsetList.Add(column + 1);
     }
+
+    private void UpdatePressLattice()
+    {
+      foreach (Lattice lattice in lastPressList)
+        lattice.Pressed = false;
+      lastPressList.Clear();
+
+      if (listBox.SelectedItem == null)
+        return;
+
+      if (leftRightBtnPressed)
+      {
+        foreach (var lattice in GetAroundLattice((Lattice)listBox.SelectedItem))
+        {
+          lattice.Pressed = true;
+          lastPressList.Add(lattice);
+        }
+      }
+      else
+      {
+        ((Lattice)listBox.SelectedItem).Pressed = true;
+        lastPressList.Add((Lattice)listBox.SelectedItem);
+      }
+    }
     private void ListBoxItem_MouseButtonUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
     {
-      //listBox.UnselectAll();
-      //listBox.SelectionMode = SelectionMode.Single;
+      leftRightBtnPressed = false;
+      listBox.UnselectAll();
+      UpdatePressLattice();
     }
     private void ListBoxItem_MouseDown(object sender, MouseButtonEventArgs e)
     {
       if (e.LeftButton == MouseButtonState.Pressed && e.RightButton == MouseButtonState.Pressed)
       {
-        listBox.SelectionMode = SelectionMode.Multiple;
-        if (listBox.SelectedItem != null)
-          foreach (var item in GetAroundLattice((int)listBox.SelectedItem))
-            listBox.SelectedItems.Add(item);
+        leftRightBtnPressed = true;
+        UpdatePressLattice();
       }
     }
     private void listBox_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
     {
-      //listBox.UnselectAll();
+      listBox.UnselectAll();
     }
 
-    private List<int> GetAroundLattice(int Lattice)
+    private List<Lattice> GetAroundLattice(Lattice lattice)
     {
-      int index = items.IndexOf(Lattice);
+      int index = items.IndexOf(lattice);
       int inRow = index / row;
-      int inColumn = Lattice % column;
-      var result = new List<int>();
+      int inColumn = index % column;
+      var result = new List<Lattice>();
       foreach (var item in offsetList)
       {
         int offset = index + item;
@@ -95,6 +121,15 @@ namespace Minesweeper
     private void Button_MouseMove(object sender, MouseEventArgs e)
     {
       listBox.UnselectAll();
+    }
+
+    private void listBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+      foreach (var item in e.RemovedItems)
+      {
+        ((Lattice)item).Pressed = false;
+      }
+      UpdatePressLattice();
     }
   }
 }
